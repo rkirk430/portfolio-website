e5a5ccdc217e5900005709e3b72bee95084e8459
rkirk430 <r.kirk430@gmail.com>
2022-05-28 17:48:59 -0400

    React Icons Set up

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
-import {BsGithub} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {FiDribbble} from 'react-icons/fi'
 
 const HeaderSocials = () => {
   return (
     <div clasname = 'header_socials'>
-        <a href = "https://linkedin.com/in/robert-kirk-1" target = "_blank"><BsLinkedin /></a>
-        <a href = "https://github.com/rkirk430" target = "_blank"><BsGithub /></a>
+        <a href = "https://www.linkedin.com/in/robert-kirk1/" target = "_blank"><BsLinkedin /></a>
+        <a href = "https://github.com/rkirk430" target = "_blank"><FaGithub /></a>
+        <a href = "https://dribble.com" target = "_blank"><FiDribbble /></a>
     </div>
   )
 }
