9f28a566646f4678f8e807383688e3d3b3d77df1
rkirk430 <r.kirk430@gmail.com>
2022-05-28 23:35:15 -0400

    Card Components About Me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {BsFolder2Open} from 'react-icons/bs'
 
 const About = () => {
   return (
-    <section id="about"> About </section>
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src = {ME} alt = "About Image" />
+          </div>
+        </div>
+
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5> Experience </h5> 
+              <small> 3+ Years Working </small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5> Clients </h5> 
+              <small> 200+ Worldwide </small>
+            </article>
+
+            <article className='about__card'>
+              <BsFolder2Open className='about__icon' />
+              <h5> Projects </h5> 
+              <small> 80+ Completed Projects </small>
+            </article>
+
+          </div>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
