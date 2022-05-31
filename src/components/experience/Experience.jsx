d9aaccb84c9820ce2e6b72fb45c3e78bde25f6a4
rkirk430 <r.kirk430@gmail.com>
2022-05-30 20:00:11 -0400

    Experience CSS Styling

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,102 @@
 import React from 'react'
 import './experience.css'
+import {BsCheckCircleFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id="experience"> Experience </section>
+    <section id="experience">j
+      <h5> What Skills I Have </h5>
+      <h2> My Experience </h2>
+      <div className='container experience__container'>
+        <div className='experience__frontend'>
+          <h3> Frontend Development </h3>
+          <div className='experience__content'>
+
+            <article className='experience__details'>
+            <BsCheckCircleFill className = 'experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+            <BsCheckCircleFill className = 'experience__details-icon' />
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+            <BsCheckCircleFill className = 'experience__details-icon' />
+              <div>
+                <h4>Javascript</h4>
+                <small className='text-light'>Experience</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+            <BsCheckCircleFill className = 'experience__details-icon' />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Experience</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsCheckCircleFill className = 'experience__details-icon' />
+              <div>
+                <h4>EJS</h4>
+                <small className='text-light'>Experience</small>
+              </div>
+            </article>
+
+          </div>
+
+        </div>
+        <div className='experience__backend'>
+        <h3> Backend Development </h3>
+          <div className='experience__content'>
+            
+            <article className='experience__details'>
+            <BsCheckCircleFill className = 'experience__details-icon' />
+              <div>
+                <h4>Node JS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+            <BsCheckCircleFill className = 'experience__details-icon' />
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+            <BsCheckCircleFill className = 'experience__details-icon' />
+              <div>
+                <h4>MySQL</h4>
+                <small className='text-light'>Experience</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+            <BsCheckCircleFill className = 'experience__details-icon' />
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Experience</small>
+              </div>
+            </article>
+
+          </div>
+
+        </div>
+      </div>
+
+    </section>
   )
 }
 
