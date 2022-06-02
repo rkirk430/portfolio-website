3afc4c6fd23a89809111e0ce1e247e63c2a194e3
rkirk430 <r.kirk430@gmail.com>
2022-06-01 20:21:12 -0400

    Footer CSS Styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './footer.css'
 import { FaFacebookF } from 'react-icons/fa'
 import { FiInstagram } from 'react-icons/fi'
-import {IoLogoTwitter} from 'react-icons/io'
+import { IoLogoTwitter } from 'react-icons/io'
 
 const Footer = () => {
   return (
