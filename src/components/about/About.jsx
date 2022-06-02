130106a34f98d21be469f04a094468838b333d42
rkirk430 <r.kirk430@gmail.com>
2022-06-01 21:20:27 -0400

    Doggo Picture Up. Needs to be refactored

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assets/me-about.jpg'
+import Doggo from '../../assets/dog.jpg'
 import {FaAward} from 'react-icons/fa'
 import {FiUsers} from 'react-icons/fi'
 import {BsFolder2Open} from 'react-icons/bs'
@@ -14,7 +14,7 @@ const About = () => {
       <div className='container about__container'>
         <div className='about__me'>
           <div className='about__me-image'>
-            <img src = {ME} alt = "About Image" />
+            <img src = {Doggo} alt = "About Image" />
           </div>
         </div>
 
